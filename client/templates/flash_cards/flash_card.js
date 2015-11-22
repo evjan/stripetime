75e363d195f6606171a75a85bbc7f01e3e47a5bc
Peter Evjan <hello@peterevjan.com>
2015-11-22 16:05:13 +1000

    Play again

--- client/templates/flash_cards/flash_card.js
+++ client/templates/flash_cards/flash_card.js
@@ -1,22 +1,23 @@
+var playVideo = function(start, end) {
+  var video = $(".flash-card_video")[0];
+
+  video.currentTime = start;
+
+  var millisecondsToWait = (end - start) * 1000;
+
+  video.play();
+  Meteor.setTimeout(function() {
+    video.pause();
+  }, millisecondsToWait);
+};
+
 Template.flashCard.events({
   "click .flash-card_video-play": function(event, template) {
-    document.querySelector('video').play();
-  },
-  "click .flash-card_video-pause": function(event, template) {
-    document.querySelector('video').pause();
+    playVideo(template.data.question_video_start, template.data.question_video_end);
   },
   "click .flash-card_show-answer": function(event, template) {
     $(".flash-card_answer").show();
 
-    var video = $(".flash-card_video")[0];
-
-    video.currentTime = template.data.question_video_end;
-
-    var millisecondsToWait = (template.data.answer_video_end - template.data.question_video_end) * 1000;
-
-    video.play();
-    Meteor.setTimeout(function(){
-      video.pause();
-    }, millisecondsToWait);
+    playVideo(template.data.question_video_end, template.data.answer_video_end);
   }
 });
