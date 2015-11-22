39fdd6fa7828e9714d7e29bdf27aa6e5b187485a
Peter Evjan <hello@peterevjan.com>
2015-11-22 15:23:29 +1000

    Removed a lot of duplication

--- client/templates/flash_cards/flash_card_edit.js
+++ client/templates/flash_cards/flash_card_edit.js
@@ -1,5 +1,5 @@
 Template.flashCardEdit.onCreated(function() {
-  Session.set('flashCardErrors', {});
+  FlashCardForm.clearErrors();
 });
 
 Template.flashCardEdit.helpers({
@@ -15,15 +15,7 @@ Template.flashCardEdit.events({
   "submit form": function(event, template) {
     event.preventDefault();
 
-    var flashCardProperties = {
-      move_name: $(event.target).find('[name=move_name]').val(),
-      video_path: $(event.target).find('[name=video_path]').val(),
-      question_text: $(event.target).find('[name=question_text]').val(),
-      answer_text: $(event.target).find('[name=answer_text]').val(),
-      question_video_start: $(event.target).find('[name=question_video_start]').val(),
-      question_video_end: $(event.target).find('[name=question_video_end]').val(),
-      answer_video_end: $(event.target).find('[name=answer_video_end]').val()
-    };
+    var flashCardProperties = FlashCardForm.properties(event);
 
     var errors = validateFlashCard(flashCardProperties);
     if (_.size(errors) > 0) {
@@ -35,13 +27,6 @@ Template.flashCardEdit.events({
     var currentFlashCardId = this._id;
     FlashCards.update(currentFlashCardId, {
       $set: flashCardProperties
-    }, function(error) {
-      if (error) {
-        throwError(error.reason);
-      } else {
-        Router.go('flashCardsAdmin');
-      }
-    });
-
+    }, FlashCardForm.saveErrorHandler);
   }
 });
--- client/templates/flash_cards/flash_card_new.js
+++ client/templates/flash_cards/flash_card_new.js
@@ -1,5 +1,5 @@
 Template.flashCardNew.onCreated(function() {
-  Session.set('flashCardErrors', {});
+  FlashCardForm.clearErrors();
 });
 
 Template.flashCardNew.helpers({
@@ -15,15 +15,7 @@ Template.flashCardNew.events({
   "submit form": function(event, template) {
     event.preventDefault();
 
-    var flashCardProperties = {
-      move_name: $(event.target).find('[name=move_name]').val(),
-      video_path: $(event.target).find('[name=video_path]').val(),
-      question_text: $(event.target).find('[name=question_text]').val(),
-      answer_text: $(event.target).find('[name=answer_text]').val(),
-      question_video_start: $(event.target).find('[name=question_video_start]').val(),
-      question_video_end: $(event.target).find('[name=question_video_end]').val(),
-      answer_video_end: $(event.target).find('[name=answer_video_end]').val()
-    };
+    var flashCardProperties = FlashCardForm.properties(event);
 
     var errors = validateFlashCard(flashCardProperties);
     if (_.size(errors) > 0) {
@@ -32,13 +24,6 @@ Template.flashCardNew.events({
       Session.set('flashCardErrors', {});
     }
 
-    FlashCards.insert(flashCardProperties, function(error) {
-      if (error) {
-        throwError(error.reason);
-      } else {
-        Router.go('flashCardsAdmin');
-      }
-    });
-
+    FlashCards.insert(flashCardProperties, FlashCardForm.saveErrorHandler);
   }
 });
