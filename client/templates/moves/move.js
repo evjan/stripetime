0a436d3316fe05af0149936c9091350b2d54ad48
Peter Evjan <hello@peterevjan.com>
2015-11-27 19:53:59 +1100

    Shows a different overlay for the answer

--- client/templates/moves/move.js
+++ client/templates/moves/move.js
@@ -10,17 +10,35 @@ Template.move.onRendered(function() {
   }).fetch();
 
   var flashCard = currentFlashCard();
-  playVideo(flashCard.question_video_start, flashCard.question_video_end);
+  playQuestion(flashCard.question_video_start, flashCard.question_video_end);
   $(".move_question").text(this.data.question_text || "What do you do next?");
 });
 
-var playVideo = function(start, end) {
+var playQuestion = function(start, end) {
   video.currentTime = start;
   video.play();
-  setTimeout(function(){
+  setTimeout(function() {
     video.pause();
     $(".move_video-overlay-question").show();
   }, (end - start) * 1000);
+};
+
+var playAnswer = function(start, end) {
+  video.currentTime = start;
+  video.play();
+  setTimeout(function() {
+    video.pause();
+    $(".move_video-overlay-answer").show();
+  }, (end - start) * 1000);
+};
+
+var hideQuestion = function() {
+  $(".move_video-overlay-question").hide();
+};
+
+var showAnswer = function() {
+  hideQuestion();
+  playAnswer(currentFlashCard().question_video_end, currentFlashCard().answer_video_end);
 };
 
 var currentFlashCard = function() {
@@ -29,7 +47,7 @@ var currentFlashCard = function() {
 
 Template.move.events({
   "click .show-answer": function(event, template) {
-    playVideo(currentFlashCard().question_video_end, currentFlashCard().answer_video_end);
+    showAnswer();
   }
 });
 
