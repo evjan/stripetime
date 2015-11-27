db2e12985a3f0d34bf5b9c3b3afd0c7de81dc8c3
Peter Evjan <hello@peterevjan.com>
2015-11-27 22:03:59 +1100

    Added toreando to knee on belly

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,3 +1,6 @@
+Moves.remove({});
+FlashCards.remove({});
+
 Moves.insert({
   title: 'Double underhooks pass',
   video_path: '/videos/Double underhooks pass.mp4'
@@ -37,7 +40,7 @@ FlashCards.insert({
   question_text: "Where do you grab?",
   question_video_start: 13,
   question_video_end: 16,
-  answer_video_end: 22,
+  answer_video_end: 23,
   answer_text: "The lapel and the outside of the leg"
 });
 FlashCards.insert({
@@ -71,40 +74,39 @@ Moves.insert({
   title: 'Submissions from closed guard',
   video_path: '/videos/Submissions from closed guard.mp4'
 });
-Moves.insert({
+
+var toreandoToKneeOnBellyId = Moves.insert({
   title: 'Toreando pass to knee on belly',
   video_path: '/videos/Toreando pass to knee on belly.mp4'
 });
+FlashCards.insert({
+  moveId: toreandoToKneeOnBellyId,
+  question_text: "How do you begin the pass?",
+  question_video_start: 5,
+  question_video_end: 7,
+  answer_video_end: 12
+});
+FlashCards.insert({
+  moveId: toreandoToKneeOnBellyId,
+  question_text: "What do you do when you get blocked?",
+  question_video_start: 7,
+  question_video_end: 13,
+  answer_video_end: 22
+});
+FlashCards.insert({
+  moveId: toreandoToKneeOnBellyId,
+  question_text: "What do you do once you have the knee on belly?",
+  question_video_start: 16,
+  question_video_end: 22,
+  answer_video_end: 26,
+  answer_text: "Stabilise and keep the other leg out so she can't grab it"
+});
+
 Moves.insert({
   title: 'Toreando pass with shoulder pressure',
   video_path: '/videos/Toreando pass with shoulder pressure.mp4'
 });
 
 if (FlashCards.find().count() === 0) {
-  // FlashCards.insert({
-  //   move_name: "Toreando pass to knee on belly",
-  //   video_path: "/videos/Toreando pass to knee on belly.mp4",
-  //   question_text: "How do you begin the pass?",
-  //   question_video_start: 5,
-  //   question_video_end: 7,
-  //   answer_video_end: 12
-  // });
-  // FlashCards.insert({
-  //   move_name: "Toreando pass to knee on belly",
-  //   video_path: "/videos/Toreando pass to knee on belly.mp4",
-  //   question_text: "What do you do when you get blocked?",
-  //   question_video_start: 7,
-  //   question_video_end: 13,
-  //   answer_video_end: 21
-  // });
-  // FlashCards.insert({
-  //   move_name: "Toreando pass to knee on belly",
-  //   video_path: "/videos/Toreando pass to knee on belly.mp4",
-  //   question_text: "What do you do once you have the knee on belly?",
-  //   question_video_start: 16,
-  //   question_video_end: 21,
-  //   answer_video_end: 26,
-  //   answer_text: "Stabilise and keep the other leg out so she can't grab it"
-  // });
 
 }
