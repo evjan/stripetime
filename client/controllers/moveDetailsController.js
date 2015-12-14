d21f9183452b71238b025ffc0315b719b876ecb7
Peter Evjan <hello@peterevjan.com>
2015-12-14 16:27:15 +1100

    Showing hardcoded YT video on move page

--- client/controllers/moveDetailsController.js
+++ client/controllers/moveDetailsController.js
@@ -14,15 +14,50 @@ stripeTime.controller('moveDetailsCtrl', function($scope, $stateParams, $meteor,
 
   $scope.showYoureDone = false;
 
-  function moveInit() {
-    $scope.firstFlashCard = true;
-    $scope.video = $document.find('.move_video');
-
-    $scope.video.one('canplay', function(event){
-      $scope.flashCards = loadFlashCards();
-      // wait til the video is loaded
-      showQuestion();
+  window.onYouTubeIframeAPIReady = function() {
+    player = new YT.Player('player', {
+      height: '390',
+      width: '640',
+      videoId: 'xDe2pUDlTx8',
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
     });
+  }
+
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  }
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function stopVideo() {
+    player.pauseVideo();
+  }
+
+  function moveInit() {
+    //$scope.firstFlashCard = true;
+    //$scope.video = $document.find('.move_video');
+    //
+    //$scope.video.one('canplay', function(event){
+    //  $scope.flashCards = loadFlashCards();
+    //  // wait til the video is loaded
+    //  showQuestion();
+    //});
   }
 
   function loadFlashCards() {
