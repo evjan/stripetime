7b40f1fb4542d90b224777891c48794303fadafc
Peter Evjan <hello@peterevjan.com>
2015-12-23 12:06:09 +0100

    Unbreaking the whole site

--- client/controllers/ytController.js
+++ client/controllers/ytController.js
@@ -1,3 +1,3 @@
-angular.module('stripetime-ng', ['youtube-embed']).controller('ytCtrl', function($scope, $meteor) {
+angular.module('stripetime-ng').controller('ytCtrl', function ($scope) {
   $scope.theBestVideo = 'sMKoNBRZM1M';
 });
