409cc99f037146bdf68dd7d5611249c655e10de3
Peter Evjan <hello@peterevjan.com>
2015-12-24 04:02:57 +0100

    Put the home controller in a function

--- client/controllers/homeController.js
+++ client/controllers/homeController.js
@@ -1,3 +1,5 @@
-angular.module('stripetime-ng').controller('homeCtrl', function($scope, $meteor) {
-  $scope.moves = $meteor.collection(Moves);
-});
+(function () {
+  angular.module('stripetime-ng').controller('homeCtrl', function ($scope, $meteor) {
+    $scope.moves = $meteor.collection(Moves);
+  });
+})();
