c615643e4b3ce5add708164e47b899f1e3cf2746
Peter Evjan <hello@peterevjan.com>
2016-01-07 20:05:32 +1100

    Redirect to the moves admin when creating a new move

--- client/controllers/moveNewController.js
+++ client/controllers/moveNewController.js
@@ -1,10 +1,11 @@
 (function () {
-  angular.module('stripetime-ng').controller('moveNewCtrl', ['$scope', function ($scope) {
+  angular.module('stripetime-ng').controller('moveNewCtrl', ['$scope', '$location', function ($scope, $location) {
     $scope.newMove = {};
 
     $scope.save = function() {
       Moves.insert($scope.newMove);
       $scope.newMove = {};
+      $location.path('/moves/admin');
     }
   }])
 })();
