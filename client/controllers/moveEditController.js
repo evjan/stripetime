0be1b641f76cb07d68814b1fc4b45fb87deab058
Peter Evjan <hello@peterevjan.com>
2016-01-09 19:32:49 +1100

    Can insert a new flash card but the video start ain't working

--- client/controllers/moveEditController.js
+++ client/controllers/moveEditController.js
@@ -1,10 +1,32 @@
-(function () {
-  angular.module('stripetime-ng').controller('moveEditCtrl', ['$scope', '$stateParams', '$meteor', '$document', '$timeout', function ($scope, $stateParams, $meteor, $document, $timeout) {
+(function() {
+  angular.module('stripetime-ng').controller('moveEditCtrl', ['$scope', '$stateParams', '$meteor', '$document', '$timeout', function($scope, $stateParams, $meteor, $document, $timeout) {
     $scope.playerVars = {
       showinfo: 0
     };
 
     $scope.move = $scope.$meteorObject(Moves, $stateParams.moveId);
+
+    var loadFlashCards = function() {
+      return $scope.$meteorCollection(function() {
+        return FlashCards.find({
+          moveId: $scope.move.$$id
+        }, {
+          sort: {
+            question_video_start: 1
+          }
+        });
+      }, false);
+    };
+
+    $scope.flashCards = loadFlashCards();
     $scope.videoId = $scope.move.video_id;
+
+    $scope.newFlashCard = {};
+
+    $scope.save = function() {
+      $scope.newFlashCard.moveId = $scope.move.$$id;
+      FlashCards.insert($scope.newFlashCard);
+      $scope.newFlashCard = {};
+    };
   }]);
 })();
